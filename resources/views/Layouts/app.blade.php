3dcd4627d0690bc8950559e545140223145f9ac1
Houssem Eddine Zerrad <zerhou@gmail.com>
2018-06-04 13:23:33 +0100

    Updated login window behavior (removed auto-close functionality imposed by Bootstrap)

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -57,63 +57,7 @@
                           <ul class="dropdown-menu dropdown-menu-left mt-2 bg-secondary">
                              <li class="px-3 py-2">
                                <!-- this is the login form -->
-                               <div class="card">
-                               <div class="card-body">
-                                   <form method="POST" action="{{ route('login') }}">
-                                       @csrf
-
-                                       <div class="form-group row">
-                                           <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('البـــريد الالكتروني') }}</label>
-
-                                           <div class="col-md-6">
-                                               <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                               @if ($errors->has('email'))
-                                                   <span class="invalid-feedback">
-                                                       <strong>{{ $errors->first('email') }}</strong>
-                                                   </span>
-                                               @endif
-                                           </div>
-                                       </div>
-
-                                       <div class="form-group row">
-                                           <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('كلــمة السر') }}</label>
-
-                                           <div class="col-md-6">
-                                               <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                               @if ($errors->has('password'))
-                                                   <span class="invalid-feedback">
-                                                       <strong>{{ $errors->first('password') }}</strong>
-                                                   </span>
-                                               @endif
-                                           </div>
-                                       </div>
-
-                                       <div class="form-group row">
-                                           <div class="col-md-6 offset-md-4">
-                                               <div class="checkbox">
-                                                   <label>
-                                                       <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('تذكرني على هذا الجهاز') }}
-                                                   </label>
-                                               </div>
-                                           </div>
-                                       </div>
-
-                                       <div class="form-group row mb-0">
-                                           <div class="col-md-8 offset-md-4">
-                                               <button type="submit" class="btn btn-primary">
-                                                   {{ __('تسجيل الدخول') }}
-                                               </button>
-
-                                               <a class="btn btn-link" href="{{ route('password.request') }}">
-                                                   {{ __('نسيت كلمة المرور؟') }}
-                                               </a>
-                                           </div>
-                                       </div>
-                                   </form>
-                               </div>
-                               </div> <!-- End of Login Card -->
+                               @include('includes.loginWindow')
                               </li>
                           </ul>
                       @else
@@ -141,8 +85,6 @@
                           </li>
                       @endguest
                     </ul>
-
-                    <!-- Middle of Navbar -->
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
