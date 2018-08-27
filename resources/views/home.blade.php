c67c91bdde1c78202cc2e691ce237dfeb628d6fd
Guemer Houssem <39636747+Houssem-Guemer@users.noreply.github.com>
2018-08-27 15:35:51 +0100

    landing page 0.15

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,14 +28,13 @@
                                 <div class="card" style="">
                                     <div class="card-content">
                                         <div class="card-body">
-                                            <h4 class="card-title">{{$event->title}}</h4>
-                                            <h6 class="card-subtitle text-muted">{{$event->location}}</h6>
+                                            <h4 class="card-title text-right">{{$event->title}}</h4>
+                                            <h6 class="card-subtitle text-muted text-right">{{$event->location}}</h6>
                                         </div>
                                         <img class="img-fluid hoverZoomLink" src="{{asset('images/EventImages/'.$event->event_image)}}" alt="{{$event->title}}">
                                         <div class="card-body">
-                                            <p class="card-text">{{$event->description}}</p>
-                                            <a href="#" class="card-link pink">اقرأ اكثر</a>
-                                            <a href="#" class="card-link pink">شيئ اخر</a>
+                                            <p class="card-text">{!!  $event->description !!}</p>
+                                            <a href="#" align="right" class="card-link pink btn btn-outline-primary float-right m-3 text-right">اقرأ اكثر</a>
                                         </div>
                                     </div>
                                 </div>
--- storage/framework/views/001ae673c6b591039e1098d80b5550aec368a10c.php
+++ storage/framework/views/001ae673c6b591039e1098d80b5550aec368a10c.php
@@ -28,14 +28,13 @@
                                 <div class="card" style="">
                                     <div class="card-content">
                                         <div class="card-body">
-                                            <h4 class="card-title"><?php echo e($event->title); ?></h4>
-                                            <h6 class="card-subtitle text-muted"><?php echo e($event->location); ?></h6>
+                                            <h4 class="card-title text-right"><?php echo e($event->title); ?></h4>
+                                            <h6 class="card-subtitle text-muted text-right"><?php echo e($event->location); ?></h6>
                                         </div>
                                         <img class="img-fluid hoverZoomLink" src="<?php echo e(asset('images/EventImages/'.$event->event_image)); ?>" alt="<?php echo e($event->title); ?>">
                                         <div class="card-body">
-                                            <p class="card-text"><?php echo e($event->description); ?></p>
-                                            <a href="#" class="card-link pink">اقرأ اكثر</a>
-                                            <a href="#" class="card-link pink">شيئ اخر</a>
+                                            <p class="card-text"><?php echo $event->description; ?></p>
+                                            <a href="#" align="right" class="card-link pink btn btn-outline-primary float-right m-3 text-right">اقرأ اكثر</a>
                                         </div>
                                     </div>
                                 </div>
