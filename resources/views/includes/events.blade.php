2947c6a04cdb4538737357b61aca12e5c7b8f7c0
Guemer Houssem <39636747+Houssem-Guemer@users.noreply.github.com>
2018-10-10 21:47:19 +0100

    events landing

--- resources/views/includes/events.blade.php
+++ resources/views/includes/events.blade.php
@@ -1,102 +1,171 @@
-<head>
-    <style>
-        .content-box-blurb>*{
-            text-overflow: ellipsis;
-            overflow: hidden;
-            font-size: 15px;
-            line-height: 22px;
-            white-space: nowrap;
-        }
-        .flip-clock-label {
-            font-size: 40px !important;
-            color: white !important;
-            text-shadow: 3px 2px black !important;
-        }
+@php
+    $card_color = '#2b53ce';
+    $news_style = 'style="background-image: linear-gradient(to bottom right, #52E5E7, #130CB7); radius: 5px;"';
 
-        .inner{
-            padding: 1rem 1rem 1rem 1rem;
-        }
-        .content-box-thumb {
-            display: block;
-            max-width: 100%;
-            height: auto;
-        }
-        .card{
-            padding:0px;
-            margin-right:-1rem;
-            padding-left: -1rem;
-        }
-    </style>
-</head>
+   if (Route::currentRouteNamed('cubs')){
+       $card_color = '#ffd700';
+       $news_style = 'style="background-image: linear-gradient(to bottom right, #FDEB71, #F8D800); radius: 5px;"';
+   }
+   if (Route::currentRouteNamed('scout')){
+       $card_color = '#00cc00';
+       $news_style = 'style="background-image: linear-gradient(to bottom right, #FFF720, #3CD500); radius: 5px;"';
+   }
 
+   if (Route::currentRouteNamed('advanced_scout')){
+       $card_color = '#701700';
+       $news_style = 'style="background-image: linear-gradient(to bottom right, #F05F57, #360940); radius: 5px;"';
+   }
+
+   if (Route::currentRouteNamed('traveler')){
+       $card_color = '#ee2c2c';
+       $news_style = 'style="background-image: linear-gradient(to bottom right, #FEB692, #EA5455); radius: 5px;"';
+   }
+@endphp
+<style>
+    .btn-light{
+        position: absolute;
+        bottom:0px;
+        left:10%;
+        background: #f2f2f2;
+        border-radius: 4px;
+        box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);
+
+        font-family: "Alarabiya Font",'Segoe UI', Tahoma, Geneva, Verdana,sans-serif !important;
+
+    }
+    .post-description-container{
+        text-align: right;
+    }
+    .post-content{
+        padding-top:35px !important;
+    }
+    .post-header>h5>span:first-child{
+        padding: 5px;
+        border-radius: 4px;
+        color:white;
+        font-family: "Alarabiya Font",'Segoe UI', Tahoma, Geneva, Verdana,sans-serif !important;
+
+    }
+    .post-header>h5>span:nth-child(2){
+        padding: 5px;
+        margin-right: 5px;
+        color:grey;
+        font-family: "Alarabiya Font",'Segoe UI', Tahoma, Geneva, Verdana,sans-serif !important;
+
+    }
+    .post-image{
+        width: 100%;
+        height: 270px;
+        margin-top: 25px;
+        border-radius: 4px;
+    }
+    .post-title>h2{
+        margin-top:20px;
+        margin-bottom:20px;
+        font-family: "Alarabiya Font",'Segoe UI', Tahoma, Geneva, Verdana,sans-serif !important;
+
+    }
+    .post-summary{
+        margin-top:10px;
+        margin-bottom:10px;
+        font-family: "Alarabiya Font",'Segoe UI', Tahoma, Geneva, Verdana,sans-serif !important;
+
+    }
+    .post-summary>p{
+        color: #444444;
+        font-weight: 400;
+        text-align: right;
+        font-size: 12pt;
+        margin: 0;
+        margin-bottom: 15px;
+        line-height: 29px;
+        height: 87px;
+        overflow: hidden;
+    }
+</style>
 <!-- events Showcases -->
+@if($events->count()>0)
+    @foreach($events as $event)
+        <div class="col-md-12 ">
+            <div class="row" dir="rtl">
 
-@if($events->count()>=3)
-    @for($event=0;$event<3;$event++)
-        <div class="col-md-4">
-            <div class="" dir="rtl">
-
-                <div class=" card col-md-12">
-
-                    <div class="inner">
-                        <img class="content-box-thumb" src="{{asset('images/EventImages/'.$events[$event]->event_image)}}" alt="tech image">
-
-                        <div class="clock{{$events[$event]->event_id}}" style="width: auto;zoom: 0.35;position: absolute;left: 25%;top: 50%"></div>
-                        <script type="text/javascript">
-                            var clock;
-
-                            $(document).ready(function() {
-
-                                // Grab the current date
-                                var currentDate = new Date();
-                                console.log(futureDate);
-                                // Set some date in the future. In this case, it's always Jan 1
-                                var futureDate  = new Date( {{substr($events[$event]->event_time, 0, 4)}}, {{substr($events[$event]->event_time, 5, 2)}}, {{substr($events[$event]->event_time, 8, 2)}}, {{substr($events[$event]->event_time, 11, 2)}}, {{substr($events[$event]->event_time, 14, 2)}});
-
-                                // Calculate the difference in seconds between the future and current date
-                                var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
-
-                                var clock = $('.clock{{$events[$event]->event_id}}').FlipClock(futureDate, {
-                                    clockFace: 'DailyCounter',
-                                    countdown: true,
-                                    language:'ar-ar',
-                                    autoStart: true
-                                });
-                            });
-                        </script>
-                        <h1 class="content-box-header ng-binding">
-                            {{$events[$event]->title}}
-                        </h1>
-                        <div class="content-box-blurb ng-binding"  >
-                            {!! $events[$event]->description !!}
-                        </div>
-
+                <div class="col-md-6">
+                    <div class="post-thumbnail">
+                        <img style="filter: blur(5px);" class="post-image" src="{{asset('images/EventImages/')}}{{"/".$event->event_image}}"  width="100%" height="100%" alt="{{$event->title}}">
                         @php
-                            $unit_name = '';
-                            if($events[$event]->unit == "cubs"){
-                                $unit_name = 'وحدة الاشبال';
-                            }
-                            if($events[$event]->unit == "sct"){
-                                $unit_name = 'وحدة الكشاف';
-                            }
-                            if($events[$event]->unit == "asct"){
-                                $unit_name = 'وحدة الكشاف المتقدم';
-                            }
-                            if($events[$event]->unit == "tvlr"){
-                                $unit_name = 'وحدة الجوالة';
-                            }
-                            if($events[$event]->unit == "cap"){
-                                $unit_name = 'وحدة القادة';
-                            }
+                            $earlier = new DateTime();
+                            $later = new DateTime(substr($event->event_time, 0, 10));
+                            $diff = $later->diff($earlier)->format("%a");
                         @endphp
-                        <div style="text-align: right !important;">
-                            <small  >{{$unit_name}}</small>
-                        </div>
+                        @if($diff == 0)
+                            <h1 class="" style="font-size: 50px;color: white;text-shadow: 3px 2px black;position: absolute;top: 50%;left: 50px;">سيتم هاذا النشاط اليوم</h1>
+                        @endif
+                        @if($diff == 1)
+                            <h1 class="" style="font-size: 50px;color: white;text-shadow: 3px 2px black;position: absolute;top: 50%;left: 50px;"> سيتم هاذا النشاط غدا </h1>
+                        @endif
+                        @if($diff == 2)
+                            <h1 class="" style="font-size: 50px;color: white;text-shadow: 3px 2px black;position: absolute;top: 50%;left: 50px;"> سيتم هاذا النشاط بعد يومين </h1>
+                        @endif
+                        @if($diff > 2 && $diff <= 10)
+                            <h1 class="" style="font-size: 50px;color: white;text-shadow: 3px 2px black;position: absolute;top: 50%;left: 50px;"> سيتم هاذا النشاط بعد  {{ $diff }} ايام </h1>
+                        @endif
+                        @if($diff > 10)
+                            <h1 class="" style="font-size: 50px;color: white;text-shadow: 3px 2px black;position: absolute;top: 50%;left: 50px;"> سيتم هاذا النشاط بعد  {{ $diff }} يوم </h1>
+                        @endif
+                    </div>
+                </div>
+                <div class=" post-content col-md-6">
+                    <div class="post-description-container">
+                        <div class="post-header">
+                            <h5>
+                                <span style="background: #7d7d8e;">
+                                    {{ $event->type }}
+                                </span>
+                                <span dir="rtl">
+                                    <i class="fa fa-clock-o"></i>
+                                    {{substr($event->event_time, 0, 10)}}
+                                </span>
+                            </h5>
 
-                        <a class="readmore-link" href="/events/{{$events[$event]->event_id}}" style="text-decoration: none">اقرأ أكثر</a>
+                        </div>
+                        <div>
+                            <div class="post-title">
+                                <h2>
+                                    {{$event->title}}
+                                </h2>
+                            </div>
+                            <div class="post-summary">
+                                {!! $event->description !!}
+                            </div>
+                            @php
+                                $unit_name = '';
+                                if($event->linked_unit == "cubs"){
+                                    $unit_name = 'وحدة الاشبال';
+                                }
+                                if($event->linked_unit == "sct"){
+                                    $unit_name = 'وحدة الكشاف';
+                                }
+                                if($event->linked_unit == "asct"){
+                                    $unit_name = 'وحدة الكشاف المتقدم';
+                                }
+                                if($event->linked_unit == "tvlr"){
+                                    $unit_name = 'وحدة الجوالة';
+                                }
+                                if($event->linked_unit == "cap"){
+                                    $unit_name= 'وحدة القادة';
+                                }
+                            @endphp
+                            <small class="card-subtitle text-right float-right" style="color: grey">{{$unit_name}}</small>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="btn btn-light " style="float:left;margin-top: 15px">
+                            <a href="/events/{{$event->event_id}}" target="_blank" style="text-decoration: none">اقرأ المزيد</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    @endfor
-    @endif
+    @endforeach
+@endif
+
