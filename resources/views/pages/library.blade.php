c6471f0333a2caa29cde122a4021dac2ae2b4b86
oussama1900 <mahgounoussama545@yahoo.fr>
2018-10-18 22:49:58 +0100

    put some changes on input field of search in books view

--- resources/views/pages/library.blade.php
+++ resources/views/pages/library.blade.php
@@ -1,11 +1,99 @@
 @extends('layouts.web_template')
 <head>
     <link rel="stylesheet" href="css/library.css">
+    <style>
+        .input-holder{
+            border-radius: 50px;
+            width: 450px;
+            position: absolute;
+            right: 28%;
+            background: rgba(0,0,0,0.8);
+            -webkit-transition: all .5s cubic-bezier(0.000, 0.105, 0.035, 1.570);
+            -moz-transition: all .5s cubic-bezier(0.000, 0.105, 0.035, 1.570);
+            transition: all .5s cubic-bezier(0.000, 0.105, 0.035, 1.570);
+            overflow: hidden;
+            height: 70px;
+        }
+        .input-holder>input{
+            width: 100%;
+            height: 50px;
+            padding: 0px 70px 0 20px;
+            opacity: 0;
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            background: transparent;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+            border: none;
+            outline: none;
+
+            font-size: 16px;
+            font-weight: 400;
+            line-height: 20px;
+            color: #FFF;
+            -webkit-transform: translate(0, 60px);
+            -moz-transform: translate(0, 60px);
+            transform: translate(0, 60px);
+            -webkit-transition: all .3s cubic-bezier(0.000, 0.105, 0.035, 1.570);
+            -moz-transition: all .3s cubic-bezier(0.000, 0.105, 0.035, 1.570);
+            transition: all .3s cubic-bezier(0.000, 0.105, 0.035, 1.570);
+            -webkit-transition-delay: 0.3s;
+            -moz-transition-delay: 0.3s;
+            transition-delay: 0.3s;
+        }
+        .input-holder>input{
+            opacity: 1;
+            -webkit-transform: translate(0, 10px);
+            -moz-transform: translate(0, 10px);
+            transform: translate(0, 10px);
+
+        }
+        .input-holder>button{
+            width: 70px;
+            height: 70px;
+            border: none;
+            border-radius: 6px;
+            background: #FFF;
+            padding: 0px;
+            outline: none;
+            position: relative;
+            z-index: 2;
+            float: right;
+            cursor: pointer;
+            -webkit-transition: all 0.3s ease-in-out;
+            -moz-transition: all 0.3s ease-in-out;
+            transition: all 0.3s ease-in-out;
+        }
+        .input-holder>button{
+            width: 50px;
+            height: 50px;
+            margin: 10px;
+            border-radius: 30px;
+
+        }
+        input{
+            -webkit-appearance: textfield;
+            border: 0px solid #ccc !important;
+
+
+            padding: 0px 70px 10px 20px !important;
+        }
+
+
+
+    </style>
 </head>
+
 @section('content')
     <div class="jumbotron" style="margin-top:-60px">
         <h1 style="color: white;font-size: 100px;text-shadow: 3px 2px black;text-align: center">كتبنا</h1>
-        <input oninput="check()" type="text" dir="rtl" name="search" placeholder="ابحث..." data-search>
+        <div class="input-holder">
+            <input oninput="check()" type="text"  dir="rtl" name="search" placeholder="ابحث..." data-search>
+            <button class="search-icon"  onclick="check()"><span class="fa fa-search"></span></button>
+        </div>
+
     </div>
     <div class="container jumbotron-fluid">
         <div class="row flex-column-reverse flex-md-row filtr-container">
