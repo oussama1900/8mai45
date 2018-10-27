1dd1b0215649058949abfc8dea79913419e2564e
Guemer Houssem <39636747+Houssem-Guemer@users.noreply.github.com>
2018-10-27 19:43:59 +0100

    coming fix v final

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Notifications\notifyCaptain;
 
 /* home controller */
 //Route::get('/', 'HomeController@index')->name('home');
-Route::get('/', function(){return view('welcome.php');})->name('home');
+Route::get('/', function(){return view('welcome');})->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/cubs', 'HomeController@cubs')->name('cubs');
 Route::get('/scout', 'HomeController@scout')->name('scout');
