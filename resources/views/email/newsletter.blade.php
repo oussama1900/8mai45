2a2effbd7e3e9d73152d9a910ca9760b6d00c37f
Guemer Houssem <39636747+Houssem-Guemer@users.noreply.github.com>
2018-10-28 23:48:16 +0100

    updating links to https

--- resources/views/email/newsletter.blade.php
+++ resources/views/email/newsletter.blade.php
@@ -37,10 +37,10 @@
                <h1 style="text-align: center">{{$title}} </h1>
             </div>
             <div style="text-align: center">
-                <img  src="www.falehscout.com/images/PostCover/{{$picture}}">
+                <img  src="https://www.falehscout.com/images/PostCover/{{$picture}}">
             </div>
             <div style="text-align: center;margin: 20px">
-                <a class="btn" href="www.falehscout.com/posts/{{$id}}"  >اقرأ أكثر</a>
+                <a class="btn" href="https://www.falehscout.com/posts/{{$id}}"  >اقرأ أكثر</a>
              </div>
         </div>
     </body>
