1a9bea4a208c67a37de45a8e91732b223f4b60d9
ghedjati oussama <ouss.ghedje@gmail.com>
2019-03-14 22:05:51 +0100

    nav and library download button updated

--- resources/views/pages/library.blade.php
+++ resources/views/pages/library.blade.php
@@ -28,6 +28,18 @@
                                 <p>{!! substr(strip_tags($book->description),0,250)."..." !!}</p>
                                 <!--here u can put a download btn like circle one, or u can make the whole card clicable-->
 
+                                <!--  -->
+                                 <div class="btn-download">
+                                      <svg width="22px" height="16px" viewBox="0 0 22 16">
+                                          <path
+                                              d="M2,10 L6,13 L12.8760559,4.5959317 C14.1180021,3.0779974 16.2457925,2.62289624 18,3.5 L18,3.5 C19.8385982,4.4192991 21,6.29848669 21,8.35410197 L21,10 C21,12.7614237 18.7614237,15 16,15 L1,15"
+                                          id="check"></path>
+                                          <polyline points="4.5 8.5 8 11 11.5 8.5" class="svg-out"></polyline>
+                                          <path d="M8,1 L8,11" class="svg-out"></path>
+                                    </svg>
+                                </div>
+                                <!--  -->
+                             
                             </div>
                         </div>
                     </div>
@@ -35,8 +47,17 @@
             @endforeach
         </div>
     </div>
+
+
 @endsection
 @section('scripts')
+     <script>
+ $(".btn-download").click(function ()
+        {
+            $(this).toggleClass("downloaded");
+        });
+</script>
+
 
 @endsection
 @section('styles')
