d9a1450f2fd4baba243752fda1fae90967e498fa
Oussama <mahgounoussama545@yahoo.fr>
2019-03-14 20:21:06 +0100

    the new footer is ready to go

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,222 +1,285 @@
 <!-- Footer -->
 <head>
-    <style>
-        .colorl {
-            color: #2b53ce;
-        }
-        .colorl:hover , .colorl:focus{
-            color: #1d3490;
-        }
-
-        .colorc {
-            color: #ffd700;
-        }
-        .colorc:hover , .colorc:focus{
-            color: #a88700;
-        }
-
-        .colors {
-            color: #00cc00;
-        }
-        .colors:hover , .colors:focus{
-            color: #008d00;
-        }
-
-        .coloras {
-            color: #701700;
-        }
-        .coloras:hover , .coloras:focus{
-            color: #cb2d00;
-        }
-
-        .colort {
-            color: #ee2c2c;
-        }
-        .colort:hover , .colort:focus{
-            color: #871919;
-        }
-        .footer{
-            background-color: #2e2e2e !important;
-            margin-top:30px;
-
-        }
-        input[type=text], select, textarea {
-            width: 100%;
-            padding: 5px;
-            border: 1px solid #ccc;
-            margin-top: 6px;
-            margin-bottom: 16px;
-            resize: vertical;
-        }
-
-        input[type=submit] {
-            background-color: #4CAF50;
-            color: white;
-
-            cursor: pointer;
-        }
-
-        input[type=submit]:hover {
-            background-color: #45a049;
-        }
-        label{
-            color:#ffffff;
-        }
-        hr{
-            background-color: #ffffff;
-        }
-        li>a{
-            color:white !important;
-        }
-        .soc-item>a>span{
-            color:white !important;
-        }
+    <style> 
+      .fab{
+        line-height: 2.2 !important;
+      }
+      .page-footer{
+          background-color:#333333 !important
+      }
+      .btn-floating{
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        cursor:pointer;
+        color: #fff;
+        
+        line-height: 39px !important;
+        background-color: #009688;
+        transition: transform 0.15s cubic-bezier(0.4, 0.0, 1, 1), opacity 0.1s 0.1s, box-shadow 0.3s;
+        opacity: 1;
+        box-shadow: 0 0 6px rgba(0, 0, 0, 0.12), 0 6px 6px rgba(0, 0, 0, 0.24);
+        z-index: 1;
+      }
+      .btn-floating:hover{
+        box-shadow: 0 0 12px rgba(0, 0, 0, 0.12), 0 12px 12px rgba(0, 0, 0, 0.24);
+      }
+      .btn-floating>i{
+          color:white;
+      }
+      .social-media-list>li{
+          margin-bottom:10px
+      }
+      .btn-fb{
+          background-color:#3b5998;
+      }
+      .btn-tw{
+          background-color:#55acee;
+      }
+      .btn-instagram{
+          background-color:#2e5e86;
+      }
+      .btn-youtube{
+          background-color:#ed302f;
+      }
+      .address .fas{
+          color:white;
+          margin-left:5px;
+      }
+      .address>li>p>small{
+        color:white;
+      }
+      .address>li>p{
+        text-align:right
+      }
+      .address-title{
+          text-align:right
+      }
+      .content>p{
+        font-size: 14px;
+        line-height: 1.64286;
+        color: #bcbec0;
+        text-align:right;
+      }
+      .content>h5{
+        color: #bcbec0;
+        text-align:right;
+      }
+      .Website-links>ul{
+          text-align:right
+      }
+      .Website-links>ul>li>p>a{
+        color: #bcbec0;
+        text-align:right;
+        text-decoration:none;
+      }
+      .Website-links>ul>li>p>a:hover{
+          color:#e3f0fd;
+          text-shadow: 2px 2px #333333;
+      }
+      .Website-links>h5{
+        color: #bcbec0;
+        text-align:right;
+      }
+      .follow-us>h5{
+        color: #bcbec0;
+      }
+      .addresses>h5{
+        color: #bcbec0;
+      }
+      .addresses>ul>li>p>small{
+        font-size: 15px;
+        line-height: 1.64286;
+        color: #bcbec0;
+      }
+      .mail-address{
+          font-size:16px !important;
+      }
+      .addresses{
+          padding-right:0;
+      }
+      .copyright{
+          color: #bcbec0;
+      }
+      .copyright>a{
+          color: #ffffff;
+      }
     </style>
 </head>
-@php
-    $color = 'colorl';
-    if (Route::currentRouteNamed('cubs')){
-        $color = 'colorc';
-    }
-    if (Route::currentRouteNamed('scout')){
-        $color = 'colors';
-    }
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color lighten-3 pt-4">
 
-    if (Route::currentRouteNamed('advanced_scout')){
-        $color = 'coloras';
-    }
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
 
-    if (Route::currentRouteNamed('traveler')){
-        $color = 'colort';
-    }
-@endphp
-<footer class="footer  p-3">
-    <section >
+      <!-- Grid row -->
+      <div class="row">
 
 
 
 
 
-        <div class="container">
-     <!--       <div class="media-container-row content mbr-white" style="display:flex">
-                <div class="col-12 col-md-6">
-                    <div style="width:100%;height:300px">
-                        <div><div><iframe style="float:left" width="400" height="300" id="" src="https://maps.google.com/maps?q=Cite%20Yahiaoui&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div><style>.mapouter{text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div>
-                    </div>                </div>
-                <div class="col-12 col-md-6 mbr-fonts-style display-7" style="margin-left:45px">
-                    <div class="column">
-                        <form action="#">
-                            <label class="float-right" for="fname" style="text-align:right">اللقب</label>
-                            <input type="text" style="text-align:right" id="fname" name="firstname" placeholder="لقبك">
-                            <label class="float-right" for="lname" style="text-align:right">الاسم</label>
-                            <input type="text" style="text-align:right" id="lname" name="lastname" placeholder="اسمك">
-                            <label class="float-right" for="subject" style="text-align:right">الموضوع</label>
-                            <textarea id="subject" style="text-align:right" name="subject" placeholder="اكتب هنا ..." style="height:170px"></textarea>
-                            <input class="float-right btn btn-success" type="submit" value="ارسل">
-                        </form>
-                    </div>
-                </div>
+
+  <!-- Grid column -->
+  <div class="col-md-2 col-lg-2 text-center mx-auto my-4 follow-us">
+
+<!-- Social buttons -->
+<h5 class="font-weight-bold text-uppercase mb-4">تابعنا</h5>
+<ul class="list-unstyled social-media-list">
+  <!-- Facebook -->
+   <li>
+      <a  class="btn btn-floating btn-fb" href="https://www.facebook.com/faleh.19/" target="_blank">
+        <i class="fab fa-facebook-f"></i>
+       </a>
+    </li>
+  
+    <li>
+     <!-- Twitter -->
+      <a class="btn btn-floating btn-tw" href="https://twitter.com/groupe_faleh_19" target="_blank">
+          <i class="fab fa-twitter"></i>
+      </a>
+    </li>
+
+    <li>
+      <!-- Google +-->
+      <a  class="btn btn-floating btn-instagram" href="https://www.instagram.com/groupfaleh/" target="_blank">
+          <i class="fab fa-instagram"></i>
+      </a>
+    </li>
+
+    <li>
+              <!-- Dribbble -->
+      <a  class="btn btn-floating btn-youtube" href="https://www.youtube.com/channel/UCvJ4gk_JufwvI9_VneFF2ZQ" target="_blank">
+          <i class="fab fa-youtube"></i>
+      </a>
+    </li>
+
+</ul>
 
 
-            </div>-->
-            <div class="footer-lower">
-                <div class="media-container-row">
 
-                </div>
-                <div class="row mbr-white">
-                    <div class="col-sm-6 copyright">
-                        <div class="mbr-text mbr-fonts-style display-7">
-                        <ul class="list-inline mb-2">
 
-                            <li class="list-inline-item">
-                                <span class="{{ $color }} text-light" >&copy; All Rights Reserved Group El-Faleh</span>
-                            </li>
-                            <li class="list-inline-item">&sdot;</li>
-                            <li class="list-inline-item">&sdot;</li>
-                            <li class="list-inline-item">
-                                <a class="{{ $color }}" href="/about">About</a>
-                            </li>
-                            <li class="list-inline-item">&sdot;</li>
 
-                            <li class="list-inline-item">
-                                <a class="{{ $color }}" href="/contact">Contact</a>
-                            </li>
-                            <li class="list-inline-item">&sdot;</li>
 
-                            <li class="list-inline-item">&sdot;</li>
-                        </ul>
-                            </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="social-list align-right">
-                            <ul class="list-inline mb-2" style="float:right">
-                                <li class="list-inline-item">
-                                    <a href="https://www.facebook.com/faleh.19" target="_blank">
-                                        <i class=" fa fa-facebook fa-2x fa-fw"></i>                                    </a>
-                                </li>
 
-                                <li class="list-inline-item">&sdot;</li>
+</div>
+<!-- Grid column -->
 
-                                <li class="list-inline-item">
-                                    <a href="https://twitter.com/groupe_faleh_19" target="_blank">
-                                        <i class=" fa fa-twitter fa-2x fa-fw"></i>                                    </a>
-                                </li>
 
-                                <li class="list-inline-item">&sdot;</li>
-                                <li class="list-inline-item">
-                                    <a href="https://www.instagram.com/groupefaleh/" target="_blank">
-                                        <i class=" fa fa-instagram fa-2x fa-fw"></i>
-                                    </a>
-                                </li>
 
-                            </ul>
 
-                    </div>
-                </div>
-            </div>
+
+
+
+
+
+
+      
+
+        <hr class="clearfix w-100 d-md-none">
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-4 mx-auto my-md-4 my-0 mt-4 mb-1 addresses">
+
+<!-- Contact details -->
+<h5 class="font-weight-bold text-uppercase mb-4 address-title"> عناويننا</h5>
+
+<ul class="list-unstyled  address">
+  <li>
+    <p dir="rtl">
+    <i class="fas fa-home " ></i> 
+    <small>              دار الشباب شخشوخ الزيتوني حي يحياوي  سطيف 
+    </small>
+   
+    </p>
+  </li>
+  <li>
+    <p dir="rtl">
+    <i class="fas fa-envelope" ></i>
+    <small class="mail-address">contact@falehscout.com</small>
+    </p>
+  </li>
+  <li>
+    <p dir="rtl" >
+   
+      <i class="fas fa-phone "  ></i> 
+      <small  dir="ltr">+ 213 336 525 457</small>
+      </p>
+  </li>
+  <li>
+    <p  dir="rtl" >
+      <i class="fas fa-mobile-alt " ></i>
+      <small dir="ltr">+ 213 780 737 357</small>
+      </p>
+  </li>
+</ul>
+
+</div>
+<!-- Grid column -->
+        <hr class="clearfix w-100 d-md-none">
+
+     
+
+
+
+
+ <!-- Grid column -->
+ <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1 Website-links">
+
+<!-- Links -->
+<h5 class="font-weight-bold text-uppercase mb-4"> الموقع</h5>
+
+<ul class="list-unstyled">
+  <li>
+    <p>
+      <a href="/">الصفحة الرئيسية</a>
+    </p>
+  </li>
+  <li>
+    <p>
+      <a href="/news">آخر الأخبار</a>
+    </p>
+  </li>
+  <li>
+    <p>
+      <a href="/library">مكتبة الفوج</a>
+    </p>
+  </li>
+  <li>
+    <p>
+      <a href="/contact">تواصل معنا</a>
+    </p>
+  </li>
+</ul>
+
+</div>
+<!-- Grid column -->
+
+
+
+        <hr class="clearfix w-100 d-md-none">
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-4 mr-auto my-md-4 my-0 mt-4 mb-1 content">
+
+          <!-- Content -->
+          <h5 class="font-weight-bold text-uppercase mb-4" >الفوج</h5>
+          <p dir="rtl" >الكشافة الاسلامية الجزائرية ك إ ج جمعية وطنية تربوية إنسانية تطوعية مستقلة ذات طابع المنفعة العمومية. </p>
+          <p dir="rtl" >فوج الفلاح للكشافة الاسلامية الجزائرية فوج كشفي مستقل تأسس سنة 1989  يخضع للقانون الاساسي الصادر عن المؤتمر الوطني والنظام الداخلي الصادر عن المجلس الوطني وينظمه النظام الداخلي الخاص به المصادق عليه من طرف مجلسه أعلى هيئة.</p>
         </div>
-    </section>
-   <!-- <div class="container">
-        <div class="row">
-            <div class="col-lg-6 text-center text-lg-left my-auto">
-                <ul class="list-inline mb-2">
-                    <li class="list-inline-item">
-                        <a class="{{ $color }}" href="/about">About</a>
-                    </li>
-                    <li class="list-inline-item">&sdot;</li>
-                    <li class="list-inline-item">
-                        <a class="{{ $color }}" href="/contact">Contact</a>
-                    </li>
-                    <li class="list-inline-item">&sdot;</li>
-                    <li class="list-inline-item">
-                        <a class="{{ $color }}" href="#">Terms of Use</a>
-                    </li>
-                    <li class="list-inline-item">&sdot;</li>
-                    <li class="list-inline-item">
-                        <a class="{{ $color }}" href="#">Privacy Policy</a>
-                    </li>
-                </ul>
-                <p class="{{ $color }} text-muted small mb-4 mb-lg-0">&copy; Your Website 2018. All Rights Reserved.</p>
-            </div>
-            <div class="col-lg-6  text-center text-lg-right my-auto">
-                <ul class="list-inline mb-0">
-                    <li class="list-inline-item mr-3">
-                        <a href="https://www.facebook.com/faleh.19" target="_blank">
-                            <i class="{{ $color }} fa fa-facebook fa-2x fa-fw"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item mr-3">
-                        <a href="https://twitter.com/groupe_faleh_19" target="_blank" >
-                            <i class="{{ $color }} fa fa-twitter fa-2x fa-fw"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="https://www.instagram.com/groupefaleh/" target="_blank" >
-                            <i class="{{ $color }} fa fa-instagram fa-2x fa-fw"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>-->
-</footer>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3 copyright">© 2019 Copyright :
+      <a href="https://falehscout.com"> falehscout.com</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
