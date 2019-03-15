e8414fa13ded02f9314e83db87b94c1fccb05f27
Oussama <mahgounoussama545@yahoo.fr>
2019-03-16 00:48:56 +0100

    contact view

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -95,6 +95,9 @@
                 margin-top: 0;
             }
         }
+        .contact-us {
+    max-width: 1307px !important;
+}
     </style>
 </head>
 
@@ -125,7 +128,7 @@
   <!--  -->
         <div class="column">
             <div style="width:100%;height:500px;">
-                <div class="mapouter"><div class="gmap_canvas"><iframe  style="margin-right:100px" width="800" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Cite%20Yahiaoui&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href=""></a></div><style>.mapouter{text-align:right;height:500px;width:500px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width: 742px;}</style></div>
+                <div class="mapouter"><div class="gmap_canvas"><iframe  style="margin-right:100px" width="650" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Cite%20Yahiaoui&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href=""></a></div><style>.mapouter{text-align:right;height:500px;width:500px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width: 742px;}</style></div>
             </div>
         </div>
         <!--  -->
@@ -144,7 +147,7 @@
                 <textarea id="subject" style="text-align:right;color: black" required name="message" placeholder="اكتب هنا ..." style="height:170px"></textarea>
                 <div class="row">
          <div class="col-lg-12">
-  <input class="btn" type="submit" value="ارسل">
+  <input class="btn submit-button" type="submit" value="ارسل">
          </div>
           
         </div>
